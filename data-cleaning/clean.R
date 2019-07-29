4023eb7b8c46440562725fe29311a5d4e4c2db99
bahadir <bhakyuz@gmail.com>
2019-07-30 00:30:05 +0200

    WIP: Clean string vector

--- data-cleaning/clean.R
+++ data-cleaning/clean.R
@@ -2,33 +2,52 @@ library(magrittr)
 fingerprint <- function(s) tolower(trimws(s))
 
 s <- c("ahmet","Ahmet","ahmet","Ahmet","Ahmet ","ahmET","Ahmet")
-s
+s2 <- s
 fingerprint(s)
-df <- data.frame(s = s, fingerprint = fingerprint(s), stringsAsFactors = F)
+cleaning <- function(s = s2){
+  df <- data.frame(s = s, fingerprint = fingerprint(s), stringsAsFactors = F)
+  
+  df <- 
+    df %>% 
+    dplyr::mutate(l = nchar(s)) %>%
+    dplyr::group_by(s) %>%
+    dplyr::mutate(count = dplyr::n()) %>%
+    dplyr::group_by(fingerprint, s) %>%
+    dplyr::mutate(count_combination = dplyr::n()) %>%
+    dplyr::group_by(fingerprint) %>% 
+    dplyr::mutate(value_to_keep = dplyr::last(s, order_by = count, )) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(will_be_updated = !value_to_keep == s) 
+  
+  # paste(df$s[!df$change], df$u[!df$change], sep = ">", collapse = " ")
+  
+  to_update <- 
+    df %>%
+    dplyr::filter(will_be_updated) %>%
+    dplyr::mutate(
+      prio_to_update = (count + count_combination * .01 + l * .001) / nrow(df),
+      final_value_to_keep = as.character(NA)
+    ) %>%
+    dplyr::arrange(dplyr::desc(prio_to_update), dplyr::desc(count), dplyr::desc(count_combination), l) %>%
+    #   dplyr::select(s, fingerprint, value_to_keep) %>% 
+    dplyr::distinct()
+  
+  for (i in 1:nrow(to_update)) {
+    to_update$final_value_to_keep[i] <- should_replace_string(
+      x = to_update$s[i], 
+      replacement = to_update$value_to_keep[i])
+  }
+  
+  df2 <- dplyr::left_join(
+    df, 
+    dplyr::select(to_update, s, final_value_to_keep),
+    by = "s")
+  
+  res <- dplyr::coalesce(df2$final_value_to_keep, df2$s)
+  return(res)
+}
 
-df <- 
-  df %>% 
-  dplyr::mutate(l = nchar(s)) %>%
-  dplyr::group_by(s) %>%
-  dplyr::mutate(count = dplyr::n()) %>%
-  dplyr::group_by(fingerprint, s) %>%
-  dplyr::mutate(count_combination = dplyr::n()) %>%
-  dplyr::group_by(fingerprint) %>% 
-  dplyr::mutate(value_to_keep = dplyr::last(s, order_by = count, )) %>%
-  dplyr::ungroup() %>%
-  dplyr::mutate(will_be_updated = !value_to_keep == s) 
-
-# paste(df$s[!df$change], df$u[!df$change], sep = ">", collapse = " ")
-
-df %>%
-  dplyr::filter(will_be_updated) %>%
-  dplyr::mutate(prio_to_update = (count + count_combination * .01 + l * .001) / nrow(df) ) %>%
-  dplyr::arrange(dplyr::desc(prio_to_update), dplyr::desc(count), dplyr::desc(count_combination), l) %>%
-#   dplyr::select(s, fingerprint, value_to_keep) %>% 
-  dplyr::distinct()
-
-
-get_user_decision <- function(x = "NoT CleAn ", replacement = "Clean"){
+should_replace_string <- function(x = "NoT CleAn ", replacement = "Clean"){
   input <- readline(prompt = paste0("Wanna replace '", x, "' with '", replacement,  "': y/n or the value desired: "))
   if(tolower(input) == "y")
   {
